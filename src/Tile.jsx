1df9cf4c88467e203fa9d5e33a8f0bb964b76cce
Oladipo Omotosho <65185499+OladipoOmotosho@users.noreply.github.com>
2024-02-25 07:33:45 +0100

    finished

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -3,35 +3,32 @@ export function Tile({ content: Content, flip, state }) {
     case "start":
       return (
         <Back
-          // className="inline-block w-16 h-16 bg-blue-300 text-center rounded-md"
-          className="inline-block w-16 h-16 md:w-32 md:h-32 bg-[#A8B4F7] text-white text-center rounded-md"
+          className="inline-block h-8 w-8 bg-blue-300 text-center"
           flip={flip}
         />
       );
     case "flipped":
       return (
-        <Front className="inline-block h-16 w-16 md:h-32 md:w-32 bg-[#6466E9] p-2 rounded-md">
+        <Front className="inline-block h-8 w-8 bg-green-500">
           <Content
-            className="text-white"
             style={{
               display: "inline-block",
               width: "100%",
               height: "100%",
-              verticalAlign: "center",
+              verticalAlign: "top",
             }}
           />
         </Front>
       );
     case "matched":
       return (
-        <Matched className="inline-block h-16 w-16 md:h-32 md:w-32 text-[#C9D2FB] p-2 rounded-md">
+        <Matched className="inline-block h-8 w-8 text-gray-300">
           <Content
-            className="text-[#C9D2FB]"
             style={{
               display: "inline-block",
               width: "100%",
               height: "100%",
-              verticalAlign: "center",
+              verticalAlign: "top",
             }}
           />
         </Matched>
@@ -42,7 +39,11 @@ export function Tile({ content: Content, flip, state }) {
 }
 
 function Back({ className, flip }) {
-  return <div onClick={flip} className={className}></div>;
+  return (
+    <div onClick={flip} className={className}>
+      ?
+    </div>
+  );
 }
 
 function Front({ className, children }) {
